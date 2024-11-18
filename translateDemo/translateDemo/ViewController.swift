d655b305cec9bc0cf8de1b8b6bce1e859cdff16d
szcck005 <tiankong@caicaikan.com>
2024-11-18 16:13:03 +0800

    添加

---
+++ translateDemo/translateDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  translateDemo
+//
+//  Created by szcck005 on 2024/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
